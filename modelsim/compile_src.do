d4c650559ba3d63059682b88ac89e1431f5e4303
dgnani <DGnani@lbl.gov>
2022-02-03 20:49:37 -0800

    create madcap top level model

--- modelsim/compile_src.do
+++ modelsim/compile_src.do
@@ -9,20 +9,14 @@ vlib work
 vmap work  
 
 #
-# Design sources
+# Design sources -- MADCAP
 #
-#vlog -incr -sv "../src/fifo_latch.sv" 
-vlog -incr -sv "../src/fifo_ff.sv" 
-#vlog -incr -sv "../src/fifo_ram.sv" 
-#vlog -incr -sv "../src/fifo_top.sv"
-vlog -incr -sv "../src/gate_posedge_clk.sv" 
-vlog -incr -sv "../src/gate_negedge_clk.sv" 
+vlog -incr -sv "../src/fifo_ff_mc.sv" 
 vlog +incdir+../testbench/tasks/ -incr -sv "../src/config_regfile_mc.sv"
-vlog -incr -sv "../src/tff.sv"
 vlog -incr -sv "../src/clk_manager_mc.sv"
-vlog -incr -sv "../src/uart_rx.sv" 
+vlog -incr -sv "../src/uart_rx_mc.sv" 
 vlog -incr -sv "../src/uart_rx_config.sv" 
-vlog -incr -sv "../src/uart_tx.sv" 
+vlog -incr -sv "../src/uart_tx_mc.sv" 
 vlog -incr -sv "../src/uart_array_rx.sv" 
 vlog -incr -sv "../src/uart_array_tx.sv" 
 vlog -incr -sv "../src/pulse_stretcher.sv" 
@@ -32,7 +26,7 @@ vlog +incdir+../testbench/tasks/ -incr -sv "../src/prbs7.sv"
 vlog -incr "../src/datapath_fsm.sv" 
 vlog -incr "../src/datapath.sv" 
 vlog -incr "../src/config_path.sv" 
-vlog -incr "../src/event_router.sv" 
+vlog -incr "../src/event_router_mc.sv" 
 vlog -incr "../src/tx_router.sv" 
 vlog +incdir+../testbench/tasks/ -incr "../src/data_packet_builder.sv" 
 vlog -incr "../src/config_packet_builder.sv" 
@@ -44,19 +38,6 @@ vlog -incr "../src/deserializer_sdr.sv"
 vlog +incdir+../testbench/tasks/ -incr "../src/comma_detect.sv" 
 vlog +incdir+../testbench/tasks/ -incr "../src/digital_core_mc.sv" 
 vlog -incr "../src/crc.sv" 
-vlog -incr "../src/crc8.sv" 
-#vlog -incr -sv "../src/fifo_rd_ctrl_async.sv" 
-#vlog -incr -sv "../src/comms_ctrl.sv" 
-#vlog -incr -sv "../src/hydra_ctrl.sv" 
-#vlog -incr -sv "../src/channel_ctrl.sv"
-#vlog -incr -sv "../src/external_interface.sv"
-vlog -incr -sv "../src/timestamp_gen.sv"
-vlog -incr -sv "../src/async2sync.sv"
-#vlog -incr -sv "../src/periodic_pulser.sv"
-#vlog -incr -sv "../src/digital_monitor.sv"
-#vlog +incdir+../testbench/tasks/ -incr -sv -L rf2p_512x64_4_50 -L tsmc_cl018g_rvt_neg -L tsmc18_cg_neg "../src/digital_core.sv"
-#vlog -L rf2p_512x64_4_50 -L tsmc_cl018g_rvt_neg -L tsmc18_cg_neg "../par/digital_core.output.v"
-#vlog -incr -sv "../src/reset_sync.sv"
 
 #
 # Standard Cells
@@ -79,6 +60,8 @@ vlog +incdir+../testbench/tasks/ -incr -sv "../testbench/unit_tests/encode8b10b_
 vlog +incdir+../testbench/tasks/ -incr -sv "../testbench/unit_tests/encode24b30b_tb.sv" 
 vlog +incdir+../testbench/tasks/ -incr -sv "../testbench/unit_tests/encode96b120b_tb.sv" 
 vlog +incdir+../testbench/tasks/ -incr -sv "../testbench/unit_tests/digital_core_mc_tb.sv" 
+vlog +incdir+../testbench/tasks/ -incr -sv "../testbench/unit_tests/madcap_tb.sv" 
+vlog +incdir+../testbench/tasks/ -incr -sv "../testbench/unit_tests/larpix_madcap_tb.sv" 
 
 #
 # aux modules for testbenches
@@ -94,7 +77,7 @@ vlog +incdir+../testbench/tasks/ -incr -sv "../testbench/sim_models/analyze_supe
 #
 # full chip model
 #
-
+vlog -incr -vopt -sv "../testbench/madcap/madcap.sv" 
 #
 # MCP
 #
---
+++ testbench/madcap/madcap.sv
@@ -0,0 +1,111 @@
+// File Name: madcap.sv
+// Engineer:  Carl Grace (crgrace@lbl.gov)
+// Description: Full-chip behavioral model for MADCAP.  
+//              Uses production synthesziable RTL.
+//              Uses real-value modeling analog circuits.
+//
+///////////////////////////////////////////////////////////////////
+`timescale 1ns/1ps
+
+module madcap
+    #(parameter FIFO_DEPTH = 32,
+    parameter REGNUM = 35,
+    parameter NUMCHANNELS = 16)
+    (output logic dout,                 // high-speed output to PACMAN
+    output logic dout_frame,            // high when LSB output
+    output logic [15:0] posi,           // config data to LArPix tiles
+    output logic [3:0] clk_larpix,      // clocks to LArPix tiles
+    output logic [3:0] reset_n_larpix,  // reset_n to LArPix tiles
+    output logic [3:0] trigger_larpix,  // triggers to LArPix tiles
+    input logic piso [NUMCHANNELS-1:0], // input bits from PHYs
+    input logic lvds_rx_bit,            // serial bits from RX (PACMAN)
+    input logic external_trigger,       // high for external trigger 
+    input logic external_sync,          // high for external sync 
+    input logic start_sync,             // start sync (also starts on rst)  
+    input logic clk_fast,               // externally supplied clk
+    input logic [2:0] chip_id,          // id for MADCAP
+    input logic reset_n);               // digital reset  (active low)
+
+// fast digital internal signals
+logic dout_even;                // DDR even bits                 
+logic dout_odd;                 // DDR odd bits  
+     
+// signals to analog core
+logic [4:0] ref_current_trim;   // trims ref current
+logic override_ref;             // high to enable external bandgap
+logic ref_kickstart;            // active high kickstart bit
+logic [3:0] current_monitor;    // one hot monitor (see docs)
+logic [7:0] voltage_monitor_refgen;  // one hot monitor 
+logic [3:0] tx_slices;          // # of TX slices for POSI link
+logic [3:0] i_tx_diff;          // TX bias current (diff)
+logic [3:0] i_rx;               // RX bias current 
+logic [4:0] rx_term;            // RX termination resistor
+logic [2:0] v_cm_tx;            // TX CM (MADCAP to tile)
+logic [3:0] i_tx_lvds_data;     // link from MADCAP to PACMAN
+logic [3:0] i_lvds_rx;          // link from PACMAN to MADCAP
+logic [3:0] i_cml_rst;          // bias current for rst drivers
+logic [3:0] i_cml_clk;          // bias current for clk drivers
+logic [3:0] i_cml_trigger;      // bias current for trigger drivers
+logic pd_lvds_tx;               // pd LVDS from MADCAP to PACMAN
+logic [3:0] pd_reset_n_drivers; // pd rst drivers to LArPix tile
+logic [3:0] pd_clk_drivers;     // pd clk drivers to LArPix tile
+logic [3:0] pd_trigger_drivers; // pd trigger to LArPix tile
+logic [15:0] pd_rx;             // pd rx from LArPix to MADCAP
+logic [15:0] pd_tx;             // pd rx from MADCAP to LArPix
+
+
+// START output mux
+output_mux  
+    output_mux_inst (
+    .dout       (dout),
+    .dout_even  (dout_even),
+    .dout_odd   (dout_odd),
+    .clk        (clk_fast)
+    );
+// END output mux
+
+// DUT connected here   
+digital_core_mc
+    #(.NUMCHANNELS(NUMCHANNELS),
+    .REGNUM(REGNUM),
+    .FIFO_DEPTH(FIFO_DEPTH))
+    digital_core_mc_inst (
+    .dout_even              (dout_even),
+    .dout_odd               (dout_odd),
+    .dout_frame             (dout_frame),
+    .posi                   (posi),
+    .clk_larpix             (clk_larpix),
+    .reset_n_larpix         (reset_n_larpix),
+    .trigger_larpix         (trigger_larpix),
+    .ref_current_trim       (ref_current_trim),
+    .override_ref           (override_ref),
+    .ref_kickstart          (ref_kickstart),
+    .current_monitor        (current_monitor),
+    .voltage_monitor_refgen (voltage_monitor_refgen),
+    .tx_slices              (tx_slices),
+    .i_tx_diff              (i_tx_diff),
+    .i_rx                   (i_rx),
+    .rx_term                (rx_term),
+    .v_cm_tx                (v_cm_tx),
+    .i_tx_lvds_data         (i_tx_lvds_data),
+    .i_lvds_rx              (i_lvds_rx),
+    .i_cml_rst              (i_cml_rst),
+    .i_cml_clk              (i_cml_clk),
+    .i_cml_trigger          (i_cml_trigger),
+    .pd_lvds_tx             (pd_lvds_tx),
+    .pd_reset_n_drivers     (pd_reset_n_drivers),
+    .pd_clk_drivers         (pd_clk_drivers),
+    .pd_trigger_drivers     (pd_trigger_drivers),
+    .pd_rx                  (pd_rx),
+    .pd_tx                  (pd_tx),
+    .piso                   (piso),
+    .lvds_rx_bit            (lvds_rx_bit),
+    .external_trigger       (external_trigger),
+    .external_sync          (external_sync),
+    .start_sync             (start_sync),
+    .clk_fast               (clk_fast),
+    .chip_id                (chip_id),
+    .reset_n                (reset_n)
+    );
+
+endmodule 
