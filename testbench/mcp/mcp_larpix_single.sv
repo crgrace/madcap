871c495be075b42255040f7b2e8525a496d88928
tarun-prakash <tprakash@lbl.gov>
2023-03-15 21:40:22 -0700

    hydra ext trigger test added

--- testbench/mcp/mcp_larpix_single.sv
+++ testbench/mcp/mcp_larpix_single.sv
@@ -175,7 +175,8 @@ initial begin
 //`include "./verification/ver_cds_minimal.mcp"
 //`include "./verification/ver_hydra_debug.mcp"
 //`include "./verification/ver_hydra_config.mcp"
-`include "./verification/ver_hydra_storm.mcp"
+//`include "./verification/ver_hydra_storm.mcp"
+`include "./verification/ver_hydra_ext_trig.mcp"
 
 //`include "lightpix_debug.mcp"
 //`include "hydra_broadcast_read.mcp"
